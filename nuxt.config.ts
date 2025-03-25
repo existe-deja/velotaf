5ad97bc6c9bee3a4df7656ed2353fb81f15e8506
Clément Procureur <mc.flurry@hotmail.fr>
2025-03-25 11:53:28 +0100

    build: initial commit for deploy branch

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-11-01',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
